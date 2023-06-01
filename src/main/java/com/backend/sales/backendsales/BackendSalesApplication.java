7de5772c202efd1a71cf27e8cc0ce55549031904
Luis Manuel Diaz <luis.diaz@fsteam.co>
2023-05-31 23:05:58 -0500

    Se inicializó el proyecto

---
+++ src/main/java/com/backend/sales/backendsales/BackendSalesApplication.java
@@ -0,0 +1,13 @@
+package com.backend.sales.backendsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/sales/backendsales/BackendSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.sales.backendsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
